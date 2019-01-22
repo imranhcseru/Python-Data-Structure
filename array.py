1cad3a579e9abfb922d44431faaf80ab8066a7dc
Imran <imranh.cse.ru@gmail.com>
2019-01-22 13:39:29 +0600

    initial commit

---
+++ array.py
@@ -0,0 +1,20 @@
+number = [10,20,30,40]
+#random indexing
+# print(number[2])
+
+# number[2] = 200
+# # print(number[2])
+
+# for num in number:
+# 	print(num)
+
+for num in range(len(number)):
+	print(number[num])
+
+print(number[:-2])
+maximum = number[0]
+for num in number:
+	if num>maximum:
+		maximum = num
+
+print(maximum)
